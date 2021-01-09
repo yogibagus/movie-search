060b78bea91d701fa836d8614ee32963d7e54a70
yogibagus <58897819+yogibagus@users.noreply.github.com>
2021-01-09 18:31:09 +0700

    Add file

---
+++ getmovie.js
@@ -0,0 +1,126 @@
+
+
+$('#search-button').on('click', function() {
+
+	getResult()
+	
+});
+
+$('#search-input').on('keyup', function(e) {
+	if (e.keyCode === 13){
+		getResult()
+	}
+});
+
+$('#movie-list').on('click','.see-details', function(e) {
+	$.ajax({
+		url : 'http://omdbapi.com',
+		type : 'get',
+		dataType : 'json',
+		data : {
+			'apikey': '91f17062',
+			'i': $(this).data('id')
+		},
+		success: function (result) {
+			if (result.Response === "True"){
+				$('.modal-body').html(`
+					<div class="container-fluid">
+						<div class="row">
+							<div class="col-md-4">
+							<img src="`+ result.Poster +`" class="img-fluid mb-3" onError="this.onerror=null;this.src='error.jpg';">
+							</div>
+							<div class="col-md-8">
+								<ul class="list-group">
+									<li class="list-group-item"><h4>`+ result.Title +`<h4></li>
+								</ul>
+								<ul class="list-group">
+									<li class="list-group-item"><b>Release Date:</b> `+ result.Released +`</li>
+								</ul>
+								<ul class="list-group">
+									<li class="list-group-item"><b>Genre:</b> `+ result.Genre +`</li>
+								</ul>
+								<ul class="list-group">
+									<li class="list-group-item"><b>Director:</b> `+ result.Director +`</li>
+								</ul>
+								<ul class="list-group">
+									<li class="list-group-item"><b>Plot:</b> `+ result.Plot +`</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					`)
+			}
+		}
+	})
+});
+
+function getResult(){
+	$('#movie-list').html('')
+
+	$.ajax({
+		url : 'http://omdbapi.com',
+		type : 'get',
+		dataType : 'json',
+		data : {
+			'apikey': '91f17062',
+			's': $('#search-input').val()
+		},
+		success: function (result) {
+			console.log(result);
+			if (result.Response == "True") {
+
+				$('#movie-total').html(`
+					<p><strong>Total result</strong> : `+ result.totalResults +` movie<br><small><i>*) Only showing max 10 movie list in a row.</i></small></p>
+				`)
+
+				$('#loading').html(`
+					<div class="row mb-5" id="covid-box">
+						<div class="col-md-12">
+							<div class="d-flex justify-content-center">
+								<div class="spinner-grow text-success" role="status">
+									<span class="sr-only">Loading...</span>
+								</div>
+								<div class="spinner-grow text-danger" role="status">
+									<span class="sr-only">Loading...</span>
+								</div>
+								<div class="spinner-grow text-warning" role="status">
+									<span class="sr-only">Loading...</span>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-12 justify-content-center">
+							<p class="text-center pt-3"><strong>Loading result...</strong></p>
+						</div>
+					</div>
+				`)
+
+				let movies = result.Search;
+
+				$.each(movies, function(i , data){
+					$('#loading').html('');
+					$('#movie-list').append(`
+						<div class="col-sm-6 col-md-6 col-lg-3">
+						<div class="card mb-3 justify-content-center">
+						<img src="`+ data.Poster +`" class="bd-placeholder-img card-img-top img-fluid miniimg" width="100%" height="225" alt="poster" onError="this.onerror=null;this.src='error.jpg';">
+						<div class="card-body d-flex flex-column">
+						<h5 class="card-title">` + data.Title + `</h5>
+						<h6 class="card-subtitle mb-2 text-muted">`+ data.Year +`</h6>
+							
+						<a href="#" class="btn btn-dark btn-block align-self-end see-details mt-auto" data-toggle="modal" data-target="#exampleModal" data-id="`+ data.imdbID +`">See details.</a>
+						</div>
+						</div>
+						</div>
+						`)
+				})
+
+			}else{
+				$('#movie-total').html('');
+				$('#movie-list').html(`
+					<div class="col-md-12">
+					<h1 class="text-center">`+ result.Error +`<h1>
+					</div>
+					`)
+			}
+		}
+	});
+}
